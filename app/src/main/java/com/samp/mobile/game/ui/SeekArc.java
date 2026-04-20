658a019f7ece505d5433894584281694a768e8c8
pretasouzamirandadesouza-maker <pretasouzamirandadesouza@gmail.com>
2026-04-20 11:55:52 -0300

    Fix package declaration in SeekArc.java

--- app/src/main/java/com/samp/mobile/game/ui/SeekArc.java
+++ app/src/main/java/com/samp/mobile/game/ui/SeekArc.java
@@ -22,7 +22,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  ******************************************************************************/
-package ru.unisamp_mobile.game.game.ui;
+package com.samp.mobile.game.ui;
 
 import android.content.Context;
 import android.content.res.Resources;
