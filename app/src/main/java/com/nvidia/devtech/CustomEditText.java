978f43d732db80d6244da82cc89f706e8b9db68f
pretasouzamirandadesouza-maker <pretasouzamirandadesouza@gmail.com>
2026-04-20 12:26:03 -0300

    Update CustomEditText.java

--- app/src/main/java/com/nvidia/devtech/CustomEditText.java
+++ app/src/main/java/com/nvidia/devtech/CustomEditText.java
@@ -1,31 +1,34 @@
 package com.nvidia.devtech;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
-@SuppressLint("AppCompatCustomView")
 public class CustomEditText extends EditText {
-    private Context mContext = null;
+
+    private Context mContext;
 
     public CustomEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
     }
 
-    public void SetBackListener(Context context) {
+    public CustomEditText(Context context) {
+        super(context);
         mContext = context;
     }
 
     @Override
     public boolean onKeyPreIme(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
-            ((NvEventQueueActivity)mContext).onEventBackPressed();
+            try {
+                ((NvEventQueueActivity) mContext).onEventBackPressed();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             return true;
         }
-        return false;
+        return super.onKeyPreIme(keyCode, event);
     }
 }
