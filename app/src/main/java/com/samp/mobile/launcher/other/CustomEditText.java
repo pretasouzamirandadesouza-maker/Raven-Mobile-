6f6af2c14de9b1faef67090c16f5d969c0f400cb
pretasouzamirandadesouza-maker <pretasouzamirandadesouza@gmail.com>
2026-04-20 12:33:26 -0300

    Update CustomEditText.java

--- app/src/main/java/com/samp/mobile/launcher/other/CustomEditText.java
+++ app/src/main/java/com/samp/mobile/launcher/other/CustomEditText.java
@@ -1,4 +1,4 @@
-package ru.unisamp_mobile.game.launcher.other;
+package com.samp.mobile.launcher.other;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -6,27 +6,32 @@ import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.widget.EditText;
 
-import ru.unisamp_mobile.game.game.SAMP;
+import com.samp.mobile.game.SAMP;
 
 @SuppressLint("AppCompatCustomView")
 public class CustomEditText extends EditText {
-    private Context mContext = null;
+
+    private Context mContext;
 
     public CustomEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
     }
 
-    public void SetBackListener(Context context) {
+    public void setBackListener(Context context) {
         mContext = context;
     }
 
     @Override
     public boolean onKeyPreIme(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
-            ((SAMP)mContext).onEventBackPressed();
+            try {
+                ((SAMP) mContext).onEventBackPressed();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             return true;
         }
-        return false;
+        return super.onKeyPreIme(keyCode, event);
     }
 }
